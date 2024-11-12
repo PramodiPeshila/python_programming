c7deb419c228625fd6b8a3ae98937b86249f3c5d
Pramodi Peshila <ppeshila7@gmail.com>
2024-11-12 23:35:19 +0530

    BMI calculator

---
+++ day2_2.py
@@ -0,0 +1,12 @@
+#height = 1.65 , weight = 84
+# 1.calculate the BMI value.
+# 2.Express BMI as an integer value.
+# 3.Round off the BMI value.
+# 4.Round off the BMI value into 2 decimal places.
+
+BMI = 84 / 1.65 ** 2
+
+print(BMI)  #30.853994490
+print(int(BMI))  #30
+print(round(BMI))  #31
+print(round(BMI, 2))  #30.85
