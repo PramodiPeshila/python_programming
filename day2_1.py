508cab88a7fd8cdf7cc9d21b1d391dc4d07743e5
Pramodi Peshila <ppeshila7@gmail.com>
2024-11-12 23:12:58 +0530

    Mathematical Operations

---
+++ day2_1.py
@@ -0,0 +1,15 @@
+# Mathematical Operations 
+
+print(1 + 2)    #3
+print(7 - 3)    #4
+print(3 * 3)    #9
+print(5 / 3)    #1.666666666667
+print(5 // 3)   #1
+print(2 ** 3)   #8
+
+
+# Order Of Mathematical Operations - PEMDAS 
+    # ()
+    # **
+    # * , /
+    # + , -
